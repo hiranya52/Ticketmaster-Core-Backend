864c261592533b5543f518cbe2b25a2682ec7544
Hiranya Mendis <hiranyamendis52@gmail.com>
2026-01-20 11:22:05 +0530

    Add Seat Entity class

--- ticketmaster-core-backend/src/main/java/edu/icet/ticketmaster/model/entity/Seat.java
+++ ticketmaster-core-backend/src/main/java/edu/icet/ticketmaster/model/entity/Seat.java
@@ -1,4 +1,33 @@
 package edu.icet.ticketmaster.model.entity;
 
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+
+import java.time.LocalDateTime;
+
 public class Seat {
+
+    @Id
+    private int id;
+
+    @JoinColumn(name = "event_id")
+    private int event;
+
+    private String seatNumber;
+
+    @Enumerated(EnumType.STRING)
+    private SeatStatus status;
+
+    private int heldByUserId;
+
+    private LocalDateTime holdExpiry;
+
+    public enum SeatStatus {
+        AVAILABLE,
+        HELD,
+        SOLD
+    }
+
 }
--- ticketmaster-core-backend/src/main/java/edu/icet/ticketmaster/repository/SeatRepository.java
+++ ticketmaster-core-backend/src/main/java/edu/icet/ticketmaster/repository/SeatRepository.java
@@ -3,5 +3,5 @@ package edu.icet.ticketmaster.repository;
 import edu.icet.ticketmaster.model.entity.Seat;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface SeatRepository extends JpaRepository<Seat, > {
+public interface SeatRepository extends JpaRepository<Seat, Integer> {
 }
