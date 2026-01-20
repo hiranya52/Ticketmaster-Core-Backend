60ff6cbcc9ae892f03e20ab78b43bb12301bddc9
Hiranya Mendis <hiranyamendis52@gmail.com>
2026-01-20 10:07:51 +0530

    Add User Entity class

--- ticketmaster-core-backend/src/main/java/edu/icet/ticketmaster/model/dto/UserDTO.java
+++ ticketmaster-core-backend/src/main/java/edu/icet/ticketmaster/model/dto/UserDTO.java
@@ -1,4 +1,32 @@
 package edu.icet.ticketmaster.model.dto;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@Entity
 public class UserDTO {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Tier tier;
+
+    private String email;
+
+    public enum Tier {
+        REGULAR,
+        VIP,
+        PLATINUM
+    }
+
 }
