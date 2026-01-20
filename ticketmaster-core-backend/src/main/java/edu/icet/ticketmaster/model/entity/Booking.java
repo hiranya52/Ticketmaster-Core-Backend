4199e829a9d406a6dec7ceaa17b97d9731ca3596
Hiranya Mendis <hiranyamendis52@gmail.com>
2026-01-20 13:15:51 +0530

    Add Booking Entity class

--- ticketmaster-core-backend/src/main/java/edu/icet/ticketmaster/model/entity/Booking.java
+++ ticketmaster-core-backend/src/main/java/edu/icet/ticketmaster/model/entity/Booking.java
@@ -7,6 +7,23 @@ import jakarta.persistence.ManyToOne;
 
 public class Booking {
 
+    @Id
+    private Long id;
 
+    @ManyToOne
+    private User user;
+
+    @ManyToOne
+    private Seat seat;
+
+    private double amountPaid;
+
+    @Enumerated(EnumType.STRING)
+    private BookingStatus status;
+
+    public enum BookingStatus {
+        CONFIRMED,
+        CANCELLED
+    }
 
 }
