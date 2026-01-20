9b0551fa651abca59bc4f28719cc27f2f3d8b83a
Hiranya Mendis <hiranyamendis52@gmail.com>
2026-01-20 10:03:47 +0530

    Add User DTO class

--- ticketmaster-core-backend/src/main/java/edu/icet/ticketmaster/model/entity/User.java
+++ ticketmaster-core-backend/src/main/java/edu/icet/ticketmaster/model/entity/User.java
@@ -1,4 +1,23 @@
 package edu.icet.ticketmaster.model.entity;
 
+import lombok.*;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
 public class User {
+
+    private int id;
+    private String name;
+    private Tier tier;
+    private String email;
+
+    public enum Tier {
+        REGULAR,
+        VIP,
+        PLATINUM
+    }
+
 }
