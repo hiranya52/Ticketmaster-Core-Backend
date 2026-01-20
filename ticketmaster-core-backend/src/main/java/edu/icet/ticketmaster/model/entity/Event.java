fb900fedf576254f89f9af61226ee2475d293f6c
Hiranya Mendis <hiranyamendis52@gmail.com>
2026-01-20 10:51:36 +0530

    Add Event Entity class

--- ticketmaster-core-backend/src/main/java/edu/icet/ticketmaster/model/entity/Event.java
+++ ticketmaster-core-backend/src/main/java/edu/icet/ticketmaster/model/entity/Event.java
@@ -1,4 +1,16 @@
 package edu.icet.ticketmaster.model.entity;
 
+import jakarta.persistence.Id;
+
+import java.time.LocalDateTime;
+
 public class Event {
+
+    @Id
+    private Long id;
+    private String name;
+    private double basePrice;
+    private boolean isHighDemand;
+    private LocalDateTime eventDate;
+
 }
